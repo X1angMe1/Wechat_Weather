f2a5a5e3161ad5ff466d0eebd0e34c352fd24141
Mei Xiang <xiangmei1986@126.com>
2018-06-04 21:42:25 +0800

    get data from weather API

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,52 @@
-Page({})
+const weatherMap = {
+  'sunny':'晴天',
+  'cloudy':'多云',
+  'overcast':'阴',
+  'lightrain':'小雨',
+  'heavyrain':'大雨',
+  'snow':'雪',
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+  data: {
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherPic:'',
+    nowNavigationBar:'',
+  },
+  onLoad(){
+    const self = this
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '北京'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        self.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          nowWeatherPic:'/images/'+weather+'-bg.png',
+          })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      }
+    })
+  }
+})
